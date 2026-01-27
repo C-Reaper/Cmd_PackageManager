48ae4213d926f344057e74172adad515707c58bc
codeleaded <gnu.c.reaper@gmail.com>
2026-01-27 15:52:59 +0100

    updated

--- src/PackageManager.c
+++ src/PackageManager.c
@@ -1,7 +1,7 @@
 #include "/home/codeleaded/System/Static/Library/PackageManager.h"
 
 int main() {
-    PackageManager pm = PackageManager_New(".","./PackageManager.yaml");
+    PackageManager pm = PackageManager_Make("./inc","./PackageManager.yaml");
     PackageManager_Load(&pm);
     PackageManager_Free(&pm);
     return 0;
